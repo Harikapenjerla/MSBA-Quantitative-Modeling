3fafd7cbc168e1565337666849e3a460aaec7441
Harikapenjerla <54416525+Harikapenjerla@users.noreply.github.com>
2019-09-22 21:28:57 -0400

    Add files via upload

---
+++ spenjerl 3.4.R
@@ -0,0 +1,23 @@
+### Dual Solution for Assignment-3
+
+library(lpSolveAPI)
+
+lprec <- read.lp("spenjerl 3.4.lp")
+
+# set boundaries for each variables
+
+set.bounds(lprec, lower=c(0, 0, 0, 0, 0, 0, 0, 0, 0, -Inf, -Inf), upper = c(Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf, Inf), columns = (1:11))   # for security model.
+
+solve(lprec)
+
+get.objective(lprec)
+
+get.variables(lprec)
+
+get.constraints(lprec)
+
+get.sensitivity.rhs(lprec)
+
+get.sensitivity.obj(lprec)
+
+get.dual.solution(lprec)
